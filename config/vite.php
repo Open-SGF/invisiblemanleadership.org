e43f5c987cc8eecdbfba5ca182ddfba79497385b
Levi Zitting <me@levizitting.com>
2023-02-14 19:24:12 -0600

    feat: add craft vite plugin

---
+++ config/vite.php
@@ -0,0 +1,14 @@
+<?php
+
+use craft\helpers\App;
+
+return [
+    'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
+    'manifestPath' => '@webroot/dist/manifest.json',
+    'devServerPublic' => 'http://localhost:3000/',
+    'serverPublic' => App::env('PRIMARY_SITE_URL') . '/dist/',
+    'errorEntry' => '',
+    'cacheKeySuffix' => '',
+    'devServerInternal' => '',
+    'checkDevServer' => false,
+];
