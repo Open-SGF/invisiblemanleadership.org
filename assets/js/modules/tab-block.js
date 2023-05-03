8b65075e74a156ec733c262ff68f5529e79e382c
jugglingdev <kaylamarie1785@gmail.com>
2023-05-02 20:24:02 -0500

    update: add JS to tab-block

---
+++ assets/js/modules/tab-block.js
@@ -0,0 +1,46 @@
+import { Module } from '../core/module';
+
+export class TabBlock extends Module {
+    selector = '.js-tab-block';
+    activeTabClass = 'active-tab';
+    activeContentClass = 'active-content';
+
+    mounted() {
+        for (const tabBlock of this.elements) {
+            console.log(tabBlock);
+            const tabs = tabBlock.querySelectorAll('.js-tab-block-tab');
+            const tabContents = tabBlock.querySelectorAll('.js-tab-block-content');
+
+            for (let index = 0; index < tabs.length; index++) {
+                console.log(index);
+                tabs[index].addEventListener('click', () => {
+                   this.removeActive(tabs, tabContents);
+                   this.addActive(tabs[index], tabContents[index]);
+                });
+            }
+        }
+    }
+
+    removeActive(tabs, tabContents) {
+        for (const tab of tabs) {
+            tab.classList.remove(this.activeTabClass);
+        }
+
+        for (const content of tabContents) {
+            content.classList.remove(this.activeContentClass);
+        }
+    }
+
+    addActive(tab, content) {
+        if(tab) {
+            tab.classList.add(this.activeTabClass);
+        }
+
+        if(content) {
+            content.classList.add(this.activeContentClass);
+        }
+    }
+}
+
+
+
